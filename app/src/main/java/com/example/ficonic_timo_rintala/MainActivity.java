53bb97d70aebea5975b6e6ec14fd1f3799c41b4b
Timo Rintala <timorintalatr@gmail.com>
2020-01-27 15:09:40 +0200

    Initial commit
    Initial commit

---
+++ app/src/main/java/com/example/ficonic_timo_rintala/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ficonic_timo_rintala;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
